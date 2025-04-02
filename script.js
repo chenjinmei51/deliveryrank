f33f2d903dd481f87b0fbecd05c9f5b9a95bac61
chenjinmei51 <chenjinmei51@gmail.com>
2025-04-03 02:52:13 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const heroTabs = document.querySelectorAll('.hero-tab');
+    const heroContents = document.querySelectorAll('.hero-content');
+
+    heroTabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            heroTabs.forEach(t => t.classList.remove('active'));
+            heroContents.forEach(content => content.classList.remove('active'));
+
+            tab.classList.add('active');
+            document.getElementById(tab.dataset.target).classList.add('active');
+        });
+    });
+});
